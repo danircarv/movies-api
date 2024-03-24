7ebf042663ce8ce77d320a3872c775a42f24d0f3
daniel <danielrodriguescarvalho777@gmail.com>
2024-03-24 08:54:07 -0300

    creating users routes

--- source/server.js
+++ source/server.js
@@ -1,18 +1,11 @@
 const express = require("express");
 
+const routes = require("./routes");
+
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-  const { id, user } = request.params;
-  response.send(`
-  Id da mensagem : ${id}.
-  Para o usuário : ${user}`);
-});
+app.use(routes);
 
-app.get("/users", (request, response) => {
-  const { page, limit } = request.query;
-
-  response.send(`Página: ${page}. Mostrar : ${limit}`);
-});
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
